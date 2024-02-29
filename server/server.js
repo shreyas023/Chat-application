f47e168cb42a90ccc00f5e21fd210a8daa378f57
LakshyaSharma207 <lakshyahero20@gmail.com>
2024-02-29 17:10:51 +0530

    fixed issue with backend not serving html

--- server/server.js
+++ server/server.js
@@ -1,4 +1,16 @@
-const io = require('socket.io')(8000)
+// running on http://localhost:8000
+
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '../')));
+
+server.listen(8000, () => {
+    console.log('Server is running on port 8000');
+});
 
 const users = {};
 
@@ -17,4 +29,4 @@ io.on('connection', socket =>{
         socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
     });
-})
+});
